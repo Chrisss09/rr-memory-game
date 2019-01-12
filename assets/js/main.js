cca114e3b92431c00068337c7716b83aa444b8d2
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-01-12 14:22:52 +0000

    Creating the gamefield function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var gameFace = ['0','0','1','1','2','2','3','3','4','4','5','5'];
+var gameFaceArray = ['0','0','1','1','2','2','3','3','4','4','5','5'];
 var gameValues = [];
 var gameTileIds = [];
 var gameFaceFlipped = 0;
@@ -11,4 +11,14 @@ Array.prototype.gameFaceShuffle = function() {
         this[j] = this[i];
         this[i] = temp;
     }
+}
+
+function newGameField() {
+    gameFaceFlipped = 0;
+    var output = '';
+    gameFaceArray.gameFaceShuffle();
+    for(var i = 0; i < gameFaceArray.length; i++) {
+        output += '<div id="tile_'+i+'" onclick="flipTile(this,\''+gameFaceArray[i]+'\')"></div>';
+    }
+    document.getElementById('game-field').innerHTML = output;
 }
