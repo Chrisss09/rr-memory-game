85d70ca52c3b27d96d159f07da34e4e82a604e97
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-01-12 13:12:09 +0000

    Creating a random shuffle method

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,12 @@ var gameValues = [];
 var gameTileIds = [];
 var gameFaceFlipped = 0;
 
+Array.prototype.gameFaceShuffle = function() {
+    var i = this.length, j, temp;
+    while(--i > 0) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
